5ab0905916d0f73f55cafc90dd4326bb37cd7474
Jacques Blom <39128232+jacquesnf@users.noreply.github.com>
2020-05-17 10:40:53 +0200

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,19 +1,82 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {StyleSheet, Text, View, TextInput as RNTextInput} from 'react-native'
+import {
+    RecoilRoot,
+    useRecoilState,
+    atom,
+    selector,
+    useRecoilValue,
+    // @ts-ignore
+} from 'recoil'
+
+const charCountState = selector({
+    key: 'charCountState',
+    get: ({get}: any) => {
+        const text = get(textState)
+        return text.length
+    },
+})
+
+function CharacterCount() {
+    const count = useRecoilValue(charCountState)
+
+    return (
+        <Text>
+            <Text style={styles.bold}>Character Count:</Text> {count}
+        </Text>
+    )
+}
+
+const textState = atom({
+    key: 'textState',
+    default: '',
+})
+
+function TextInput() {
+    const [text, setText] = useRecoilState(textState)
+
+    return (
+        <View>
+            <RNTextInput style={styles.textInput} value={text} onChangeText={setText} />
+            <View style={styles.spacer} />
+            <Text>
+                <Text style={styles.bold}>Echo:</Text> {text}
+            </Text>
+        </View>
+    )
+}
+
+const Counter: React.FC = () => (
+    <View style={styles.container}>
+        <TextInput />
+        <View style={styles.spacer} />
+        <CharacterCount />
+    </View>
+)
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+    return (
+        <RecoilRoot>
+            <Counter />
+        </RecoilRoot>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        padding: 20,
+    },
+    textInput: {
+        padding: 16,
+        paddingBottom: 14,
+        borderWidth: 1,
+        borderColor: '#3578e5',
+        borderRadius: 3,
+    },
+    bold: {
+        fontWeight: '600',
+    },
+    spacer: {
+        height: 20,
+    },
+})
